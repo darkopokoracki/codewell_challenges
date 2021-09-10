6ec59dea3f450f8d9e2d89501e21c9c281609a2d
darkopokoracki <darkopokoracki@gmail.com>
2021-09-10 15:42:11 +0200

    Mobile Menu Completed

--- chrip_landing_page/javascript/script.js
+++ chrip_landing_page/javascript/script.js
@@ -1,7 +1,22 @@
 //Selectors
-let hamburgerMenu = document.querySelector('.hamburger-menu img');
-
-//Events
+const hamburgerMenu = document.querySelector('.hamburger-menu img');
+const mobileMenu = document.querySelector('.mobile');
+const closeButton = document.querySelector('.close');
 
 
 //Functions
+function openHamburgerMenu() {
+    mobileMenu.classList.remove('d_none');
+    mobileMenu.classList.add('mobile-menu');
+}
+
+function closeHamburgerMenu() {
+    mobileMenu.classList.add('d_none');
+}
+
+
+//Events
+hamburgerMenu.addEventListener('click', openHamburgerMenu);
+closeButton.addEventListener('click', closeHamburgerMenu);
+
+
