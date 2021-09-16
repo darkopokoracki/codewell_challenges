d8da93461a7a973861e5fbee86dd336f8cbde23a
darkopokoracki <darkopokoracki@gmail.com>
2021-09-16 20:25:50 +0200

    Spense Landing Page - Tablet Version Completed

--- spense_landing_page/app.js
+++ spense_landing_page/app.js
@@ -1,12 +1,12 @@
 const burger = document.querySelector('.burger');
-const navList = document.querySelector('.nav-list');
+const navMobile = document.querySelector('.nav-mobile');
 const line1 = document.querySelector('.line1');
 const line2 = document.querySelector('.line2');
 const line3 = document.querySelector('.line3');
 
 
 function burgerMenu() {
-    navList.classList.toggle('mobile-active');
+    navMobile.classList.toggle('mobile-active');
     line1.classList.toggle('line1-active');
     line2.classList.toggle('line2-active');
     line3.classList.toggle('line3-active');
